9c4e617d182d16ddf80d4d482d2ef89aa631658b
XanderYe <XanderYe@outlook.com>
2022-03-12 21:28:51 +0800

    feat: 支持缩小到托盘

--- src/main/java/cn/xanderye/Main.java
+++ src/main/java/cn/xanderye/Main.java
@@ -1,12 +1,18 @@
 package cn.xanderye;
 
+import cn.xanderye.ext.JavaFxWindow;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.io.IOException;
+import java.net.URL;
 
 /**
  * @author XanderYe
@@ -23,7 +29,29 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("/Main.fxml"));
         primaryStage.setTitle("空白背景音");
         primaryStage.setScene(new Scene(root, 400, 150));
+        primaryStage.setResizable(false);
         primaryStage.show();
-        primaryStage.setOnCloseRequest(event -> System.exit(1));
+        primaryStage.iconifiedProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue) {
+                Platform.runLater(primaryStage::hide);
+            }
+        });
+        primaryStage.setOnCloseRequest(event -> System.exit(0));
+
+        Platform.setImplicitExit(false);
+        URL url = Thread.currentThread().getContextClassLoader().getResource("favicon.png");
+        MenuItem open = new MenuItem("打开");
+        open.addActionListener(e -> JavaFxWindow.showStage(primaryStage));
+        MenuItem close = new MenuItem("关闭");
+        close.addActionListener(e -> System.exit(0));
+        JavaFxWindow.initTray(url, "打开窗口", new MenuItem[]{open, close}, new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                //鼠标左键
+                if (e.getButton() == MouseEvent.BUTTON1) {
+                    JavaFxWindow.showStage(primaryStage);
+                }
+            }
+        });
     }
 }
